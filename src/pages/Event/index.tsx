6f271c37135abba9a6f6fc82e5f7caa157cbd36b
Anderson Begossi <andersonbegossi@gmail.com>
2020-08-29 11:16:56 -0300

    add routes

---
+++ src/pages/Event/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Event: React.FC = () => {
+  return <h1>Event</h1>;
+}
+
+export default Event;
---
+++ src/routes/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import Home from '../pages/Home';
+import Event from '../pages/Event';
+
+const Routes: React.FC = () => (
+  <Switch>
+    <Route path="/" exact component={Home}/>
+    <Route path="/event" component={Event}/>
+  </Switch>
+);
+
+export default Routes;
